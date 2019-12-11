8fb7d6ae3069fcca6aacbbae534cf196be8a202b
Riccardo Mancini <r.mancini@santannapisa.it>
2019-12-11 18:19:57 +0100

    Added independent spawning of VIPs and normal clients

--- src/customerSpawn.cc
+++ src/customerSpawn.cc
@@ -5,31 +5,65 @@ Define_Module(CustomerSpawn);
 
 void CustomerSpawn::initialize()
 {
-    pVip = par("pVip").doubleValue();
     pCompound = par("pCompound").doubleValue();
 
-    TimerModule::initialize();
+    vipOffset = par("vipOffset").doubleValue();
+    vipInterval = 1/par("vipRate").doubleValue();
+    normalInterval = 1/par("normalRate").doubleValue();
+    randFunc = par("randFunc").stringValue();
 
-    scheduleTimer();
+    vipTimer = new cMessage("vipTimerMsg");
+    normalTimer = new cMessage("normalTimerMsg");
+
+    scheduleTimer(vipTimer, vipInterval+vipOffset);
+    scheduleTimer(normalTimer, normalInterval);
 }
 
 void CustomerSpawn::handleMessage(cMessage *msg)
 {
-    if (msg->isName("timerMsg")){
-        handleTimerMessage(msg);
+    if (msg->isName("vipTimerMsg")){
+        handleTimerMessage(msg, true);
+    } else if (msg->isName("normalTimerMsg")){
+        handleTimerMessage(msg, false);
     } else{
         EV << "Received unknown message: " << msg->getName() << endl;
     }
 }
 
-void CustomerSpawn::handleTimerMessage(cMessage *msg)
+void CustomerSpawn::handleTimerMessage(cMessage *msg, bool vip)
 {
     Order* order = new Order("orderMsg");
-    order->setVip(pVip >= uniform(0,1));
+    order->setVip(vip);
     order->setCompound(pCompound >= uniform(0,1));
     order->setCreationTime(simTime());
 
     send(order, "out");
 
-    scheduleTimer();
+    if (vip){
+        scheduleTimer(vipTimer, vipInterval);
+    } else {
+        scheduleTimer(normalTimer, normalInterval);
+    }
+}
+
+
+void CustomerSpawn::scheduleTimer(cMessage* timer, double avgInterval)
+{
+    double interval;
+    if (randFunc.compare("const") == 0){
+        interval = avgInterval;
+    } else if (randFunc.compare("exp") == 0){
+        interval = exponential(avgInterval);
+    } else {
+        EV << "Unrecognized random function: " << randFunc << endl;
+        return;
+    }
+
+    scheduleAt(simTime() + interval, timer);
+}
+
+void CustomerSpawn::finish()
+{
+    cancelAndDelete(normalTimer);
+    cancelAndDelete(vipTimer);
 }
--- src/customerSpawn.h
+++ src/customerSpawn.h
@@ -2,19 +2,25 @@
 #define __FINAL_PROJECT_CUSTOMERSPAWN_H_
 
 #include <omnetpp.h>
-#include "timerModule.h"
 
 using namespace omnetpp;
 
-class CustomerSpawn : public TimerModule
+class CustomerSpawn : public cSimpleModule
 {
   private:
-    double pVip;
+    double vipInterval;
+    double vipOffset;
+    double normalInterval;
     double pCompound;
+    std::string randFunc;
+    cMessage* normalTimer;
+    cMessage* vipTimer;
   protected:
     virtual void initialize() override;
+    virtual void finish() override;
     virtual void handleMessage(cMessage *msg) override;
-    void handleTimerMessage(cMessage *msg);
+    void handleTimerMessage(cMessage *msg, bool vip);
+    void scheduleTimer(cMessage* timer, double interval);
 };
 
 #endif
