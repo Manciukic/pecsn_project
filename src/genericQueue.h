f3d09bc1d7d0ab70d8babdf6efcc72ef21fc9ba6
zaccaria97 <51884612+zaccaria97@users.noreply.github.com>
2019-12-23 21:36:34 +0100

    Update genericQueue.h

--- src/genericQueue.h
+++ src/genericQueue.h
@@ -10,6 +10,7 @@ class GenericQueue {
   public:
     virtual Order* next() = 0;
     virtual void push(Order* order) = 0;
+    virtual double getQueueLength(bool vip) = 0;
 };
 
 #endif /* GENERICQUEUE_H_ */
